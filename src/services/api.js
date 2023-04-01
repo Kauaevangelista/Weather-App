7af389f8ef471d4ee3d19077a6691287e762436a
Kauaevangelista <kaua13evangelista@gmail.com>
2023-04-01 15:07:21 -0300

    ;)

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 const KEY = '56d8346e189b4416bd4142745230104';
 
 const fetchData = async (city) => {
-  const url = `http://api.weatherapi.com/v1/current.json?key=${KEY}&q=${city}&aqi=no`;
+  const url = `https://api.weatherapi.com/v1/current.json?key=${KEY}&q=${city}&aqi=no`;
 
   const fetchResponse = await fetch(url);
   const data = await fetchResponse.json();
